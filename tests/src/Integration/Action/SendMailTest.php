99aa06aa2b0726fdb464900a70519f63cfb3aafd
Kalman Hosszu <hosszu.kalman@gmail.com>
2014-11-01 11:03:08 +0100

    Test summary for send mail action.

--- tests/src/Integration/Action/SendMailTest.php
+++ tests/src/Integration/Action/SendMailTest.php
@@ -1,1 +1,37 @@
 <?php
+
+namespace Drupal\Tests\rules\Integration\Action;
+
+use Drupal\Tests\rules\Integration\RulesIntegrationTestBase;
+
+/**
+ * @coversDefaultClass \Drupal\rules\Plugin\Action\SystemMailSend
+ * @group rules_actions
+ */
+class SendMailTest extends RulesIntegrationTestBase  {
+
+  /**
+   * The action to be tested.
+   *
+   * @var \Drupal\rules\Engine\RulesActionInterface
+   */
+  protected $action;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+
+    $this->action = $this->actionManager->createInstance('rules_mail_send');
+  }
+
+  /**
+   * Tests the summary.
+   *
+   * @covers ::summary()
+   */
+  public function testSummary() {
+    $this->assertEquals('Send mail', $this->action->summary());
+  }
+}
